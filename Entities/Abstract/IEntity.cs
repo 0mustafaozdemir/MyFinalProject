18d0016d1b64500a137724285757fdf4e0bab076
musta <musta@DESKTOP-D7EE2NB>
2021-04-17 14:19:56 +0300

    Proje dosyası ekle.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntitiy İmplement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
